dfffba0a850d1d4cc7ce8c286258f38e79d2ed1b
Felix Song <qing.song@siemens.com>
2018-05-17 00:52:11 +0800

    fix #3 issue

--- HouseMarketTracker/spiders/LianjiaSpider.py
+++ HouseMarketTracker/spiders/LianjiaSpider.py
@@ -33,35 +33,7 @@ class LianjiaSpider(scrapy.Spider):
             meta = {}
             meta['item'] = item
             meta['root_url'] = root_url
-            yield from ParseUtil.start_request(root_url + "xiangqing/", DetailParser().parse, meta)
-
-
-class HouseLayoutParser():
-
-    def parse(self, response):
-        meta = response.meta
-        item = meta['item']
-
-        layout_li_s = response.xpath('//li[@class="huxing-item"]')
-        layout_list = []
-        for layout_li in layout_li_s:
-            layout_dict = {}
-            layout_dict['layout_type_name'] = layout_li.xpath('a/span/text()').extract_first()
-            layout_dict['layout_image_url'] = layout_li.xpath('a/div/img/@src').extract_first()
-
-            info_clear_div = layout_li.xpath('div')
-            for row in info_clear_div.xpath('ul/li/text()').extract():
-                res = re.search(r'(.+?)：(.+)', row)
-                key = res.group(1)
-                value = res.group(2)
-                layout_dict[key] = value
-            layout_dict['price'] = ''.join(info_clear_div.xpath('span[@class="price"]//text()').extract())
-            layout_dict['sales_status'] = info_clear_div.xpath('span[@class="status on-sell"]/text()').extract_first()
-            layout_list.append(layout_dict)
-        item['house_layout'] = layout_list
-
-        # print(item.keys())
-        yield item
+            yield from ParseUtil.start_request(root_url, HouseHomePageParser().parse, meta)
 
 
 class ImagesParser():
@@ -84,8 +56,9 @@ class ImagesParser():
             image_dict[title] = image_list
         item['house_images'] = image_dict
 
-        house_layout_url = meta['root_url'] + 'huxingtu/'
-        yield from ParseUtil.start_request(house_layout_url, HouseLayoutParser().parse, meta)
+        yield item
+        # house_layout_url = meta['root_url'] + 'huxingtu/'
+        # yield from ParseUtil.start_request(house_layout_url, HouseLayoutParser().parse, meta)
         # print(meta)
 
 
@@ -186,7 +159,7 @@ class DetailParser():
 
     def parse_pre_sales(self, response):
         pre_sales_list = []
-        tr_s = response.xpath('/html/body/div[4]/div[1]/table/tbody/tr')
+        tr_s = response.xpath('//tr')
         th_list = []
         for th_text in tr_s[0].xpath('*/text()').extract():
             th_list.append(th_text)
@@ -199,6 +172,50 @@ class DetailParser():
         return pre_sales_list
 
 
+class HouseHomePageParser():
+
+    def parse(self, response):
+        meta = response.meta
+        item = meta['item']
+        item['house_layout'] = self.parse_layout(response)
+
+        house_detail_url = meta['root_url'] + 'xiangqing/'
+        print(house_detail_url + '---------------------------------')
+        yield from ParseUtil.start_request(house_detail_url, DetailParser().parse, meta)
+
+    def parse_layout(self, response):
+        layout_list = []
+        layout_dict = {}
+
+        layout_ul_s = response.xpath('//ul[@class="clear house-det"]')
+        for layout_ul in layout_ul_s:
+            img_src = layout_ul.xpath('child::*/img/@src').extract_first()
+            img_src = re.sub(r'\d+?x\d*', '1000x', img_src)
+            layout_dict['img_src'] = img_src
+            layout_dict['layout_type_name'] = layout_ul.xpath('child::*/span/text()').extract_first()
+
+            info_li = layout_ul.xpath('li[@class="info-li"]')
+            p1 = info_li.xpath('p[@class="p1"]')
+            layout_dict['layout_type'] = p1.xpath('text()').extract_first()
+            layout_dict['construction_area'] = p1.xpath('span[not(@class)]/text()').extract_first()
+            layout_dict['sales_status'] = p1.xpath('span[contains(@class,"p1-state")]/text()').extract_first()
+
+            p2 = info_li.xpath('p[@class="p2"]')
+
+            layout_dict['layout_price'] = re.sub(r'\n.+', '', info_li.xpath('string(p[@class="p2"])').extract_first())
+            layout_dict['last_update_time'] = p2.xpath('span[contains(@class,"p2-time")]/text()').extract_first()
+
+            p3 = info_li.xpath('p[@class="p3"]')
+            key = p3.xpath('text()').extract_first()
+            value = p3.xpath('span/text()').extract_first()
+            layout_dict[key] = value
+
+            layout_dict['tags'] = info_li.xpath('p[@class="p4"]/span/text()').extract()
+            layout_list.append(layout_dict)
+        return layout_list
+
+    def parse_unit_info(self, response):
+        return
 # class HouseInfoParser():
 #     detail_parser = DetailParser()
 #     images_parser = ImagesParser()
